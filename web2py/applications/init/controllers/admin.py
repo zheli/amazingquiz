e49a9a3ec4d639bba9be6bd0009bc254089241a0
Zhe <linuxcity.jn@gmail.com>
2010-11-15 16:23:10 +0100

    fixed setup()

--- web2py/applications/init/controllers/admin.py
+++ web2py/applications/init/controllers/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8
 
-@auth.requires_login()
-#@auth.requires_membership('group name')
+#@auth.requires_login()
+@auth.requires_membership('admins')
 def index():
     session.characters = db().select(db.characters.ALL, orderby=db.characters.id)
     form = SQLFORM(db.characters)
@@ -13,10 +13,6 @@ def index():
 
 @auth.requires_login()
 def setup():
-    try:
-        admin_group_ID = auth_add_group('admins', 'the root!')
-        auth.add_membership(admin_group_ID)
-    except:
-        pass
-
+    admin_group_ID = auth.add_group('admins', 'the root!')
+    auth.add_membership(admin_group_ID)
     return True
